9da23435db729811183870c7ba72040e7108fafb
dj-dedek <dedekitsjay@gmail.com>
2020-06-15 19:49:21 +0700

    Calculator with Class

--- novice/01-01/Latihan/HelloWorld/Main.java
+++ novice/01-01/Latihan/HelloWorld/Main.java
@@ -5,14 +5,53 @@ import java.util.Scanner;
 
 public class Main
 {
-    static int a,b;
-    static int hasil;
-    static char operator;
+    public int a,b;
+    public int hasil;
+    public char operator;
 
+    public static int penjumlahan(int a, int b) {
+        
+        int hasil = a + b;
+
+        return hasil;
+    }
+
+    public static int pengurangan(int a, int b) {
+        
+        int hasil = a - b;
+
+        return hasil;
+    }
+
+    public static int pembagian(int a, int b) {
+        
+        int hasil = a / b;
+
+        return hasil;
+    }
+
+    public static int perkalian(int a, int b) {
+        
+        int hasil = a * b;
+
+        return hasil;
+    }
+
+    public static int sisaBagi(int a, int b) {
+        
+        int hasil = a % b;
+
+        return hasil;
+    }
+    
+    // public static int hasil(int hasil) {
+    //     hasil = 
+    // }
     public static void operatorWrong() {
         System.out.println("Operasi yang anda gunakan salah!");
         // Output.s("Test");
     }
+
     public static void main(String[] args)
     {
         // args[0] = "test";
@@ -29,9 +68,10 @@ public class Main
 
         // a operator b 
 
-        // int a,b,hasil;
-        // char operator;
-        // Main cal = new Main();
+        int a,b;
+        // hasil;
+        char operator;
+        Main cal = new Main();
         Scanner s = new Scanner(System.in);
 
         System.out.print("Nilai a  : ");
@@ -42,33 +82,51 @@ public class Main
         b = s.nextInt();
 
         System.out.println("Input user : " + a + " " + operator + " " + b);
-
+        
+        switch(operator) {
+            case '+' : 
+                       System.out.println("Hasilnya adalah : " + penjumlahan(a, b));
+                break;
+            case '/' : 
+                       System.out.println("Hasilnya adalah : " + pembagian(a, b));
+                break;
+            case '*' : 
+                       System.out.println("Hasilnya adalah : " + perkalian(a, b));
+                break;
+            case '-' : 
+                       System.out.println("Hasilnya adalah : " + pengurangan(a, b));
+                break;
+            case '%' : 
+                       System.out.println("Hasilnya adalah : " + sisaBagi(a, b));
+                break;
+            default : operatorWrong();
+        }
 
         // operasi penjumlahan
-        if(operator == '+') {
-            hasil = a + b;
-            System.out.println("Hasilnya adalah : " + hasil);
+        // if(operator == '+') {
+        //     hasil = a + b;
+        //     System.out.println("Hasilnya adalah : " + hasil);
 
-        // operasi pengurangan    
-        } else if(operator == '-'){
-            hasil = a - b ;
-            System.out.println("Hasilnya adalah : " + hasil);
+        // // operasi pengurangan    
+        // } else if(operator == '-'){
+        //     hasil = a - b ;
+        //     System.out.println("Hasilnya adalah : " + hasil);
 
-        // operasi perkalian   
-        } else if(operator == '*'){
-            hasil = a * b ;
-            System.out.println("Hasilnya adalah : " + hasil);
+        // // operasi perkalian   
+        // } else if(operator == '*'){
+        //     hasil = a * b ;
+        //     System.out.println("Hasilnya adalah : " + hasil);
 
-        // operasi pembagian   
-        } else if(operator == '/'){
-            hasil = a / b ;
-            System.out.println("Hasilnya adalah : " + hasil);
-        } else if(operator == '%'){
-            hasil = a % b ;
-            System.out.println("Hasilnya adalah : " + hasil);
-        } else{
-            operatorWrong();
-        }
+        // // operasi pembagian   
+        // } else if(operator == '/'){
+        //     hasil = a / b ;
+        //     System.out.println("Hasilnya adalah : " + hasil);
+        // } else if(operator == '%'){
+        //     hasil = a % b ;
+        //     System.out.println("Hasilnya adalah : " + hasil);
+        // } else{
+        //     operatorWrong();
+        // }
 
     }
 }
